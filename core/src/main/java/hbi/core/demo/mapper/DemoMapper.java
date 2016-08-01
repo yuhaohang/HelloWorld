3a5768ec3e678a0bc92299e5fe82c782f76e5cad
YuHaoHang <724862075@qq.com>
2016-08-01 14:43:33 +0800

    logs

---
+++ core/src/main/java/hbi/core/demo/mapper/DemoMapper.java
@@ -0,0 +1,10 @@
+package hbi.core.demo.mapper;
+
+import com.hand.hap.mybatis.common.Mapper;
+import hbi.core.demo.dto.Demo;
+
+/**
+ * Created by hailor on 16/6/2.
+ */
+public interface DemoMapper extends Mapper<Demo> {
+}
---
+++ core/src/main/java/hbi/core/demo/service/impl/DemoServiceImpl.java
@@ -0,0 +1,18 @@
+package hbi.core.demo.service.impl;
+
+import hbi.core.demo.dto.Demo;
+import hbi.core.demo.service.IDemoService;
+
+import com.hand.hap.core.IRequest;
+import com.hand.hap.system.dto.DTOStatus;
+import com.hand.hap.system.service.impl.BaseServiceImpl;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class DemoServiceImpl extends BaseServiceImpl<Demo> implements IDemoService {
+
+}
